8436d760cad4246189671f6c871b66a4d12c3b1c
MarcelloGhiozzi <marcelloghiozzi@Computer.local>
2017-06-26 22:33:12 +0200

    better demo

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -3,25 +3,40 @@ import {ImageCropService} from '../src';
 
 @Component({
   selector: 'demo-app',
-  template: '<p><img [src]="image.src"/></p><p><img [src]="cropped.src"/></p>'
+  templateUrl: 'demo.html',
 })
 export class Demo {
 
 	url: string;
   	image: HTMLImageElement;
   	cropped: HTMLImageElement;
+  	sx: number;
+  	sy: number;
+  	w: number;
+  	h: number;
 
 	constructor(private cropper:ImageCropService){
+		this.sx = 0;
+		this.sy = 0;
+		this.w = 500;
+		this.h = 500;
 		console.log(cropper);
 		this.image = new Image();
 		this.cropped = new Image();
 		this.image.onload = (event) => {
-			this.cropper.crop(this.image,250,100,150,100)
-			.then(b64 => {
-				this.cropped.src = b64;
-			})
-			.catch(err => console.log(err));
+			this.doCrop(this.sx,this.sy,this.w,this.h);
 		}
 		this.image.src = 'kiddo.jpg';
+	}
+
+
+	doCrop(sx,sy,w,h){
+		console.log(sx,sy,h,w);
+		this.cropper.crop(this.image,sx,sy,w,h)
+		.then(b64 => {
+			//console.log(b64);
+			this.cropped.src = b64;
+		})
+		.catch(err => console.log(err));
 	}
 }
